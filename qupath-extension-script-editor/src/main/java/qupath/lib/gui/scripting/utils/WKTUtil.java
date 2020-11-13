7a21b24866ff920c0be507b7577a02548c880bab
qinhangyu <acproject@qq.com>
2020-11-13 23:32:45 +0800

    update build

--- qupath-extension-script-editor/src/main/java/qupath/lib/gui/scripting/utils/WKTUtil.java
+++ qupath-extension-script-editor/src/main/java/qupath/lib/gui/scripting/utils/WKTUtil.java
@@ -1,24 +1,25 @@
 package qupath.lib.gui.scripting.utils;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.io.WKTReader;
-import org.geotools.geojson.geom.GeometryJSON;
+
+
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.io.WKTReader;
 
 import java.io.StringWriter;
 
 public class WKTUtil {
-
-    public static String wktToJson(String wkt) {
-        String json = null;
-        try {
-            WKTReader reader = new WKTReader();
-            Geometry geometry = reader.read(wkt);
-            StringWriter writer = new StringWriter();
-            GeometryJSON g = new GeometryJSON(20);
-            g.write(geometry, writer);
-            json = writer.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return json;
-    }
+//
+//    public static String wktToJson(String wkt) {
+//        String json = null;
+//        try {
+//            WKTReader reader = new WKTReader();
+//            Geometry geometry = reader.read(wkt);
+//            StringWriter writer = new StringWriter();
+////            GeometryJSON g = new GeometryJSON();
+////            g.write(geometry, writer);
+//            json = geometry.toText();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return json;
+//    }
 }
