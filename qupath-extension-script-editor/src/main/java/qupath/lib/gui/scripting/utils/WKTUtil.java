d60ad66b8d448d8fe290dcd9f40a26215880b3a2
qinhangyu <acproject@qq.com>
2020-11-14 10:53:21 +0800

    update script project

--- qupath-extension-script-editor/src/main/java/qupath/lib/gui/scripting/utils/WKTUtil.java
+++ qupath-extension-script-editor/src/main/java/qupath/lib/gui/scripting/utils/WKTUtil.java
@@ -1,25 +1,27 @@
 package qupath.lib.gui.scripting.utils;
 
 
+
+import org.geotools.geojson.geom.GeometryJSON;
 import org.locationtech.jts.geom.Geometry;
 import org.locationtech.jts.io.WKTReader;
 
 import java.io.StringWriter;
 
 public class WKTUtil {
-//
-//    public static String wktToJson(String wkt) {
-//        String json = null;
-//        try {
-//            WKTReader reader = new WKTReader();
-//            Geometry geometry = reader.read(wkt);
-//            StringWriter writer = new StringWriter();
-////            GeometryJSON g = new GeometryJSON();
-////            g.write(geometry, writer);
-//            json = geometry.toText();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        return json;
-//    }
+
+    public static String wktToJson(String wkt) {
+        String json = null;
+        try {
+            WKTReader reader = new WKTReader();
+            Geometry geometry = reader.read(wkt);
+            StringWriter writer = new StringWriter();
+            GeometryJSON g = new GeometryJSON();
+            g.write(geometry, writer);
+            json = geometry.toText();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return json;
+    }
 }
